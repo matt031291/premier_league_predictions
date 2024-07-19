d3ea22418dd6a48099ed46438f80184ef6ed38af
Matthew Price-Williams <matt@Matts-MacBook-Pro.local>
2024-07-19 18:39:32 +0200

    remove create database function

--- app.py
+++ app.py
@@ -429,7 +429,7 @@ def create_league():
 
 
 if __name__ == '__main__':
-    create_database()
+    #create_database()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
