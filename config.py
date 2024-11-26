733391a0d485fb371f62f180b4227ce41d2689ce
Matthew Price-Williams <matt@Matts-MacBook-Pro.local>
2024-11-26 13:32:29 +0000

    remove password details by changing base user to master

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgresql://matt0312:2536Flying@database-premier-league-predictions.cdeacqwaa9qe.eu-north-1.rds.amazonaws.com:5432/test')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgresql://master:master@database-premier-league-predictions.cdeacqwaa9qe.eu-north-1.rds.amazonaws.com:5432/test')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
