28bbe43efc591e083e91268c976b128fca53223f
Matthew Price-Williams <matthewpricewilliams@gmail.com>
2024-12-02 16:53:36 +0000

    extending to ios

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_jwt_extended import JWTManager, create_access_token
 from werkzeug.security import generate_password_hash, check_password_hash
 import numpy as np
 import random 
@@ -477,6 +478,25 @@ def create_league():
 
     flash('League created successfully!', 'success')
     return redirect(url_for('admin'))
+
+
+
+@app.route('/loginIOS', methods=['POST'])
+def loginIOS():
+
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        token = create_access_token(identity=username)
+        return jsonify(access_token=token), 200
+    return jsonify({"msg": "Invalid username or password"}), 401
+
+
+
+
+
 
 
 if __name__ == '__main__':
